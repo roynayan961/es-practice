ab11e1e94cc9b2b7bcebcfea172ad6569d92e85c
roynayan961 <roynayan961@gmail.com>
2020-08-25 11:48:01 +0600

    commit3

---
+++ backup1.js
@@ -0,0 +1,7 @@
+function add (num1, num2){   // num2 is declared as backup variable but  u did not mention backup value at num2
+num2 = num2 || 20;           // variable will find backup value here
+return num1+num2;
+
+}
+const total = add(15)  // if u wrote add(15,17) then this function would be executed as 15+17, its not matter u have written any backup value in main function {} 
+console.log(total)
